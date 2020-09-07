79f84951333929d4581015056505ae6ee6311a61
Dominique Revuz <nimdanor@github.com>
2020-09-07 17:31:01 +0200

    correcting ^ycodestyle problems

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,17 +1,22 @@
 from django.db import models
-from django.db.models.signals import post_save 
-from django.dispatch import receiver 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from playactivity.models import Activity
+from playexo.models import Answer, LoggedPLSession
+
+# creation des modèles stockant les valeurs calculées
+#
+# a partir des valeurs de answers dans playexo
+
+class ActivityStats:
+    pla_id = models.ForeignKey(Activity)
 
 
-
-# creation des modèles stockant les valeurs calculées a partir des valeurs de answers dans playexo
-from playexo.models import Answer
-
-
-@receiver(post_save,sender=Answer)
+@receiver(post_save, sender=Answer)
 def answerhook(sender , instance: Answer , created, *args, **kwargs):
 
-    print("Answer created")
+    if type(instance.session) != LoggedPLSession :
+            print(" rien a battre ")
 
-    raise(False)
+    plid=    instance.session.pl_id
+    userid= instance.session.pl_id
