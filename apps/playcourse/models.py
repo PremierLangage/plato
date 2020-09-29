42089bf1a5fc36cad8cf1cee03b97c8f5a713bf5
Dominique Revuz <dominique.revuz@gmail.com>
2020-09-29 21:52:09 +0200

    Update models.py

--- apps/playcourse/models.py
+++ apps/playcourse/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class Course(models.Model):
     pass
+
+# doit contenir les stats du cours dans un json field avec un système de cache
+# rafraichissement toutes les heures (seulement) 
+# Stats :
+#    nbhits : nombre d'answer du cours (tous les validate)
+#    avghits: nbhiots //nbstudents
+#    nbstudents : len(students())
+#    nbhitsperPltp :
