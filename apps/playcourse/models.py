5529c395e5e56ae94ce9fa720f8d875b49a53d5d
Dominique Revuz <dominique.revuz@gmail.com>
2020-09-30 09:42:33 +0200

    Update models.py

--- apps/playcourse/models.py
+++ apps/playcourse/models.py
@@ -8,7 +8,10 @@ class Course(models.Model):
 # doit contenir les stats du cours dans un json field avec un système de cache
 # rafraichissement toutes les heures (seulement) 
 # Stats :
-#    nbhits : nombre d'answer du cours (tous les validate)
-#    avghits: nbhiots //nbstudents
-#    nbstudents : len(students())
-#    nbhitsperPltp :
+#    nbhits : nombre d'answer du cours (tous les "validate" sans erreur), RECURSIF
+#    nbexos: nombre d'exercices, RECURSIF 
+#    avghits: (nbhits//nbstudents) du cours
+#    nbstudents : len(course.students())
+#    avgavg: moyenne des moyenne des sous activités 
+#    avgmax: Moyenne des grade max 
+#    
